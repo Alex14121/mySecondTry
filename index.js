ff679d0d070006702943f962b9ed4009c1a135dd
Olek <oleksandr.tarabesh@gmail.com>
2022-05-25 20:00:34 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/* const objectA = {
+    a: 10,
+    b: true
+}
+
+const copyOfA = objectA
+
+objectA.c = 12 * 2
+
+console.log(copyOfA.c) */
+
+/* const myCity  = {
+    city: 'New York'
+}
+myCity.city = 'Kryvyi Rih'
+
+myCity.popular = true
+myCity.length = '124 km'
+myCity.safty = false  */
+
+console.log(1 +59);

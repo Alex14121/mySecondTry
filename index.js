81c26e8a3bdc62c610467cec4070abe6b47f3171
Olek <oleksandr.tarabesh@gmail.com>
2022-05-26 14:04:59 +0300

    personalMoveDB

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const copyOfA = objectA
 
 objectA.c = 12 * 2
 
-console.log(copyOfA.c) */
+console.log(copyOfA.c)
 
 /* const myCity  = {
     city: 'New York'
@@ -18,4 +18,31 @@ myCity.popular = true
 myCity.length = '124 km'
 myCity.safty = false  */
 
-console.log(1 +59);
+//console.log(1 +59);
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMoveDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''), 
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');  
+
+
+personalMoveDB.movies[a] = b;
+personalMoveDB.movies[c] = d;
+
+console.log(personalMoveDB);
+
+
+
+
+
